c4ccd064463e4c7212fa38a1cf54d118315f5a2d
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-01-30 16:44:12 -0600

    User Authentication

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
@@ -41,6 +41,27 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+
 def signout(request):
     logout(request)
     return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username and password did not match'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
+        
