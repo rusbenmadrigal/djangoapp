1697328055fd7c335f885b1b7d6395bfe3851394
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-02 23:11:10 -0600

    Listing tasks by user

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -62,6 +62,28 @@ def create_task(request):
             return render(request, 'create_task.html', {
                 'form': TaskForm,
                 'error': 'Bad data passed in. Try again.'
+            })
+
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'GET':
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {
+            'task': task,
+            'form': form
+        })
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+                'task': task,
+                'form': form,
+                'error': 'Bad info'
             })
 
 
