5092e3953ec092daf35ce647ade35f1dc340a295
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-02-03 18:10:16 -0600

    Update

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -39,13 +41,18 @@ def signup(request):
             'error': 'Passwords did not match'
         })
 
-
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
 
     return render(request, 'tasks.html', {'tasks': tasks})
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
 
+    return render(request, 'tasks.html', {'tasks': tasks})    
 
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -63,8 +70,7 @@ def create_task(request):
                 'form': TaskForm,
                 'error': 'Bad data passed in. Try again.'
             })
-
-
+@login_required
 def task_detail(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'GET':
@@ -86,7 +92,22 @@ def task_detail(request, task_id):
                 'error': 'Bad info'
             })
 
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
 
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
